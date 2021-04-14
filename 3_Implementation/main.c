1ce49a6e426871a96c017c93c1948100efbae210
Gundapu Lakshman <80638240+Lakshman265055@users.noreply.github.com>
2021-04-14 20:04:46 +0530

    Create main.c

---
+++ 3_Implementation/main.c
@@ -0,0 +1,36 @@
+
+#include "student.h"
+
+
+void main()
+{
+    int i,j,choice;
+    printf("*************************Student Managemnt System********************************\n\n\n");
+    printf("**********************************Menu**********************************\n\n");
+    printf("1. Add new Student\n");
+    printf("2. Search Student\n");
+    printf("3. Modify Student Record\n");
+    printf("4. Delete Student Record\n");
+    printf("5. Exit\n\n");
+    printf("Enter your selection: \n");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                Add();
+                break;
+            case 2:
+                Search();
+                break;
+            case 3:
+                Modify();
+                break;
+            case 4:
+                Delete();
+                break;
+            case 5:
+                exit(0);
+                break;
+            default:
+                break;
+        }
+}
