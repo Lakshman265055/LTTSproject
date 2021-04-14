d052127af77a5b6e0dc2a15d8c444947dc04a39e
Gundapu Lakshman <80638240+Lakshman265055@users.noreply.github.com>
2021-04-14 20:44:42 +0530

    Update test_student_management.c

--- 3_Implementation/test/test_student_management.c
+++ 3_Implementation/test/test_student_management.c
@@ -5,11 +5,8 @@
 #define PROJECT_NAME    "Student"
 
 /* Prototypes for all the test functions */
-void test_add(void);
-void test_add_testcase2(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
+void test_stud(void);
+
 
 /* Required by the unity test framework */
 void setUp(){}
@@ -23,11 +20,7 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_add);
-  RUN_TEST(test_add_testcase2);
-  RUN_TEST(test_subtract);
-  RUN_TEST(test_multiply);
-  RUN_TEST(test_divide);
+  RUN_TEST(test_stud);
 
   /* Close the Unity Test Framework */
   return UNITY_END();
@@ -35,33 +28,6 @@ int main()
 
 /* Write all the test functions */ 
 void test_add(void) {
-  TEST_ASSERT_EQUAL(30, add(10, 20));
-  TEST_ASSERT_EQUAL(-10, add(10, -20));
-  TEST_ASSERT_EQUAL(-30, add(-10, -20));
-  TEST_ASSERT_EQUAL(10, add(-10, 20));
-}
-void test_add_testcase2(void) {
- 
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(1500, add(750, 7500));
-}
-void test_subtract(void) {
-  TEST_ASSERT_EQUAL(-3, subtract(0, 3));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(1, subtract(1000, 900));
-}
+  TEST_ASSERT_EQUAL(SUCCESS, add());
 
-void test_multiply(void) {
-  TEST_ASSERT_EQUAL(0, multiply(1, 0));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(2, multiply(2, 5));
-}
-
-void test_divide(void) {
-  TEST_ASSERT_EQUAL(0, divide(1, 0));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(3, divide(2, 2));
 }
