affee00a0a0eec1d9ab7c1678369317bc39e4bb0
Gundapu Lakshman <80638240+Lakshman265055@users.noreply.github.com>
2021-04-15 18:50:39 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,4 +1,13 @@
-
+/**
+ * @file main.c
+ * @author Lakshman 265055
+ * @brief Main program of the project
+ * @version 0.1
+ * @date 2021-04-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include "student.h"
 
 
